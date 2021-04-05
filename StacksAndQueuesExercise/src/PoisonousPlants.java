9ec43ab6f972ababbf6b7567a69eb02be1e18d3e
lstamenov <stamenovl99@gmail.com>
2021-04-05 21:07:36 +0300

    Added problems 1-7 from Stacks And Queues Exercise

---
+++ StacksAndQueuesExercise/src/PoisonousPlants.java
@@ -0,0 +1,9 @@
+import javax.swing.plaf.synth.SynthTextAreaUI;
+import java.util.Scanner;
+
+public class InfixToPostfix {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
